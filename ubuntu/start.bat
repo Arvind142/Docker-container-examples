a6cd2af101100921a6a652e260abe42fba101f43
Arvind Choudhary <arvindchoudhary142@yahoo.in>
2022-04-16 21:48:09 +0530

    .bat added for windows execution

---
+++ ubuntu/start.bat
@@ -0,0 +1,7 @@
+# to run ubuntu image
+docker run -d -it --name ubuntu-container ubuntu
+
+echo "#########################################################################"
+echo "to connect to bash shell of ubuntu container type below given command"
+echo "docker exec -it ubuntu-container bash"
+echo "#########################################################################"
